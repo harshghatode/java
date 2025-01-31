a04dec6c5fa9f154cf9012159ccbc5c1a7e60403
harshghatode <harshghatode1000@gmail.com>
2025-02-01 04:00:33 +0530

    Day1-java

--- wipro_java_project/src/com/wipro/java/Main.java
+++ wipro_java_project/src/com/wipro/java/Main.java
@@ -6,21 +6,27 @@ package com.wipro.java;
 /**
  * 
  */
+class MyClass {
+    int num = 5;
+ 
+    public void changeValue(int num) {
+        this.num = num; //current class variable get changed to new one
+    }
+}
+
 public class Main {
 
-	/**
-	 * 
-	 */
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MyClass obj = new MyClass();// the object has been created
+        obj.changeValue(10); //calling method
+        System.out.println(obj.num); //it will print object of my class
 
 	}
 
 }
+
+	/**
+	 * 
+	 */
+	
