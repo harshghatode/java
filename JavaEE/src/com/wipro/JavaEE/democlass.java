a22b85a617210d9f8b1aff22171e818569ee4f90
harshghatode <harshghatode1000@gmail.com>
2025-01-31 21:14:37 +0530

    commit from dev

---
+++ JavaEE/src/com/wipro/JavaEE/democlass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.wipro.JavaEE;
+
+/**
+ * 
+ */
+public class democlass {
+
+	/**
+	 * 
+	 */
+	public democlass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my JavaEE project");
+
+	}
+
+}
